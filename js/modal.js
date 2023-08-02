5d12a6dbd3a28c8d942c52d40bb960d2ef2a03e1
YuriiM-Owl <129115158+YuriiM-Owl@users.noreply.github.com>
2023-08-02 18:02:56 +0300

    done 06-hw

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle('scroll-lock');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
